42583aeeb3d8e37f879e3a96b33284d83c2ed5a3
SiPhung953 <siphung953@gmail.com>
2024-12-17 20:26:44 +0700

    Updated Student class: Add mark and str method

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -3,6 +3,7 @@ class Students:
         self.__id = input("Student ID: ")
         self.__name = input("Name: ")
         self.__dob = input("Dob: ")
+        self.__mark = {}
 
     def get_id(self):
         return self.__id
@@ -10,5 +11,22 @@ class Students:
         return self.__name
     def get_dob(self):
         return self.__dob
+    def set_mark(self, course_id, mark):
+        self.__mark[course_id] = mark
+    def get_mark(self):
+        return self.__mark
     
-class 
+    def __str__(self):
+        return f'Name: {self.__name}, ID: {self.__id}, DoB: {self.__dob}'
+        
+    
+class Course:
+    def __init__(self):
+        self.__id = input("Course ID: ")
+        self.__name = input("Course Name: ")
+
+    def get_id(self):
+        return self.__id
+    def get_name(self):
+        return self.__name
+    
